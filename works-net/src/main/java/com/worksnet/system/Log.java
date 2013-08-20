91f7365c2921bc7fa90cd06e2bd9eac4f991023e
maxim.levicky <max.levicky@gmail.com>
2013-08-20 16:28:29 +0400

    Add slf4j support in Log

--- works-net/src/main/java/com/worksnet/system/Log.java
+++ works-net/src/main/java/com/worksnet/system/Log.java
@@ -1,18 +1,27 @@
 package com.worksnet.system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+
 /**
  * @author Max Levicky
  *         Date: 13.04.13
  *         Time: 20:40
  */
 public class Log {
+    private static final HashMap<String, Logger> loggers = new HashMap<>();
 
-    public static void log(String message) {
-
-    }
-
-    public static void log(String message, Throwable throwable) {
-        System.out.println(message);
-        throwable.printStackTrace();
+    public static Logger getLogger(Class clazz) {
+        String name = clazz.getName();
+        if (!loggers.containsKey(name)) {
+            synchronized (loggers) {
+                if (!loggers.containsKey(name)){
+                    loggers.put(name, LoggerFactory.getLogger(name));
+                }
+            }
+        }
+        return loggers.get(name);
     }
 }
